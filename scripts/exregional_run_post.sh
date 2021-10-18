6a0284de7374c273ced0df3446f1769663ed14d2
eric james <eric.james@noaa.gov>
2021-10-18 21:30:42 +0000

    Per suggestion from Ming, we will do interpolation of ARI GRIB2 files offline and link them in within exregional_run_post.sh.

--- scripts/exregional_run_post.sh
+++ scripts/exregional_run_post.sh
@@ -293,65 +293,65 @@ if [ ${NET} = "RRFS_CONUS" ] || [ ${NET} = "RRFS_NA_3km" ] || [ ${NET} = "RRFS_N
     wgrib2 latest.FFG -match "0-3 hour" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ffg_03h.grib2
     wgrib2 latest.FFG -match "0-1 hour" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ffg_01h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa100yr01ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa100yr01ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari100y_01h.grib2
+  if [ -f ${FIX_UPP}/ari100y_01h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari100y_01h_${NET}.grib2 ./ari100y_01h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa10yr01ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa10yr01ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari10y_01h.grib2
+  if [ -f ${FIX_UPP}/ari10y_01h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari10y_01h_${NET}.grib2 ./ari10y_01h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa5yr01ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa5yr01ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari5y_01h.grib2
+  if [ -f ${FIX_UPP}/ari5y_01h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari5y_01h_${NET}.grib2 ./ari5y_01h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa2yr01ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa2yr01ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari2y_01h.grib2
+  if [ -f ${FIX_UPP}/ari2y_01h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari2y_01h_${NET}.grib2 ./ari5y_01h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa100yr03ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa100yr03ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari100y_03h.grib2
+  if [ -f ${FIX_UPP}/ari100y_03h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari100y_03h_${NET}.grib2 ./ari100y_03h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa10yr03ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa10yr03ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari10y_03h.grib2
+  if [ -f ${FIX_UPP}/ari10y_03h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari10y_03h_${NET}.grib2 ./ari10y_03h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa5yr03ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa5yr03ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari5y_03h.grib2
+  if [ -f ${FIX_UPP}/ari5y_03h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari5y_03h_${NET}.grib2 ./ari5y_03h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa2yr03ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa2yr03ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari2y_03h.grib2
+  if [ -f ${FIX_UPP}/ari2y_03h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari2y_03h_${NET}.grib2 ./ari5y_03h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa100yr06ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa100yr06ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari100y_06h.grib2
+  if [ -f ${FIX_UPP}/ari100y_06h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari100y_06h_${NET}.grib2 ./ari100y_06h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa10yr06ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa10yr06ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari10y_06h.grib2
+  if [ -f ${FIX_UPP}/ari10y_06h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari10y_06h_${NET}.grib2 ./ari10y_06h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa5yr06ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa5yr06ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari5y_06h.grib2
+  if [ -f ${FIX_UPP}/ari5y_06h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari5y_06h_${NET}.grib2 ./ari5y_06h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa2yr06ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa2yr06ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari2y_06h.grib2
+  if [ -f ${FIX_UPP}/ari2y_06h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari2y_06h_${NET}.grib2 ./ari5y_06h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa100yr12ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa100yr12ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari100y_12h.grib2
+  if [ -f ${FIX_UPP}/ari100y_12h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari100y_12h_${NET}.grib2 ./ari100y_12h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa10yr12ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa10yr12ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari10y_12h.grib2
+  if [ -f ${FIX_UPP}/ari10y_12h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari10y_12h_${NET}.grib2 ./ari10y_12h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa5yr12ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa5yr12ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari5y_12h.grib2
+  if [ -f ${FIX_UPP}/ari5y_12h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari5y_12h_${NET}.grib2 ./ari5y_12h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa2yr12ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa2yr12ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari2y_12h.grib2
+  if [ -f ${FIX_UPP}/ari2y_12h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari2y_12h_${NET}.grib2 ./ari5y_12h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa100yr24ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa100yr24ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari100y_24h.grib2
+  if [ -f ${FIX_UPP}/ari100y_24h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari100y_24h_${NET}.grib2 ./ari100y_24h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa10yr24ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa10yr24ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari10y_24h.grib2
+  if [ -f ${FIX_UPP}/ari10y_24h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari10y_24h_${NET}.grib2 ./ari10y_24h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa5yr24ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa5yr24ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari5y_24h.grib2
+  if [ -f ${FIX_UPP}/ari5y_24h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari5y_24h_${NET}.grib2 ./ari5y_24h.grib2
   fi
-  if [ -f ${ARI_DIR}/allusa2yr24ha.st4grid2_update3.grb2 ]; then
-    wgrib2 ${ARI_DIR}/allusa2yr24ha.st4grid2_update3.grb2 -match "0-1 day" -end -new_grid_interpolation bilinear -new_grid_winds grid -new_grid ${grid_specs_rrfs} ari2y_24h.grib2
+  if [ -f ${FIX_UPP}/ari2y_24h_${NET}.grib2 ]; then
+    cp_vrfy ${FIX_UPP}/ari2y_24h_${NET}.grib2 ./ari5y_24h.grib2
   fi
 fi
 #
